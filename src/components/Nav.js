f0f2843b79a5ba655aefae12dd7a6a730043097c
JhohannesK <jdomeh77@gmail.com>
2022-04-03 18:34:04 +0000

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Nav() {
+      return (
+            <div>
+                  <div>
+                        <p>KD MOVIE HUB</p>
+                  </div>
+            </div>
+      )
+}
+
+export default Nav
